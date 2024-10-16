ae163062de1878dcda3a6d78c75d58214a6d08ad
Lifehack25 <126723076+Lifehack25@users.noreply.github.com>
2024-10-16 15:29:14 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Circle(int radie)
+    {
+        public int Radie = radie;
+
+        public double GetArea(int radie)
+        {
+            double area = radie * radie * Math.PI;
+            return area;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            double areaCircle1 = circle1.GetArea(circle1.Radie);
+            double areaCircle2 = circle2.GetArea(circle2.Radie);
+
+            Console.WriteLine($"Area of circle 1: {areaCircle1}\nArea of circle 2: {areaCircle2}");
+        }
+    }
+}
